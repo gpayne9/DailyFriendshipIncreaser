54d68f35b104060f0e00cedd6e2be1cb04a7e7ed
Guy <gpayne9@uncc.edu>
2016-03-22 11:57:31 -0400

    no message

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using StardewValley;
+using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMod
+{
+    public class MyfirstMod : Mod
+    {
+        public override string Name
+        {
+            get
+            { return "MyFirstMod"; }
+        }
+
+        public override string Authour
+        {
+            get
+            { return "Guy Payne"; }
+        }
+        public override string Version
+        {
+            get
+            { return "0.1"; }
+        }
+        public override string Description
+        {
+            get
+            { return "Not sure yet"; }
+        }
+        public override void Entry(params object[] objects)
+        {
+            TimeEvents.DayOfMonthChanged += Event_ChangedDayOfMonth;
+        }
+        static void Event_ChangedDayOfMonth(object sender, EventArgs e)
+        {
+                
+
+        }
+    }
+}
